787d34208adaf6574e55975e656f52f2d790b1ef
Federico GP Ferreri <blues.fede@gmail.com>
2021-08-26 17:18:07 +0200

    aggiunto foto utili alla relazione finale, aggiornato ddl

--- bd/ddl_progetto.sql
+++ bd/ddl_progetto.sql
@@ -35,36 +35,31 @@ drop table if exists Allergia;
 
 /* DEFINIZIONE TABLE DDL*/
 
-create sequence SequenzaVaccinando
-increment by 1 start 1;
-create domain Vacc_id as integer default nextval('SequenzaVaccinando');
 create table Vaccinando (
+	IdVacc char(8) not null ,
 	CF char(16) not null ,
 	Nome varchar(20) not null ,
 	Cognome varchar(20) not null ,
-	Eta smallint not null check (eta > 0),
+	Data_nascita date not null ,
 	Città varchar(20) not null ,
 	Indirizzo varchar(30) not null ,
-	Categoria varchar(21) 
-		check (	categoria = 'PERSONALE SANITARIO' or
-			  	categoria = 'PERSONALE SCOLASTICO' or
-			  	categoria = 'SOGGETTO FRAGILE' or
-			  	categoria = 'ALTRO') not null ,
-	Positività_pregressa boolean not null ,
-	IdVacc Vacc_id,
+	Categoria char(4) 
+		check (	categoria = 'PSAN' or
+			  	categoria = 'PSCO' or
+			  	categoria = 'SFRA' or
+			  	categoria = 'ALTR') not null ,
+	Positività_pregressa boolean default 'false' ,
 	constraint pk_vaccinando primary key (IdVacc),
 	unique (CF)
 );
 
-create sequence SequenzaCentro
-increment by 1 start 1;
-create domain Centro_id as integer default nextval('SequenzaCentro');
+
 create table Centro_Vaccinale (
+	IdCentro char(8) not null ,
 	Nome varchar(20) not null ,
 	Citta varchar(20) not null ,
 	Indirizzo varchar(30) not null ,
-	CentroId Centro_id,
-	constraint pk_centro primary key (CentroId),
+	constraint pk_centro primary key (IdCentro),
 	unique (Nome, Citta)
 );
 
@@ -74,104 +69,80 @@ create table Vaccino (
 	Eta_min smallint not null check (eta_min > 0),
 	Eta_max smallint not null check (eta_max > 0 and eta_max > eta_min),
 	Int_dosi smallint check (int_dosi > 0),
-	Tipologia varchar(13) 
-			check (	tipologia = 'SINGOLA DOSE' or
-				  	tipologia = 'DOPPIA DOSE') not null ,
+	Tipologia char 
+			check (	tipologia = 'S' or
+				  	tipologia = 'D') not null ,
 	constraint pk_vaccino primary key (Nome)
 );
 
 create table Allergia (
-	Nome varchar(15) not null ,
+	Nome varchar(20) not null ,
 	constraint pk_allergia primary key (nome)
 );
 
 create table Effetto_allergico (
-	Nome varchar(15) not null ,
+	Nome varchar(20) not null ,
 	constraint pk_effetto primary key (nome)
 );
 
-create sequence SequenzaMedico
-increment by 1 start 1;
-create domain Medico_id as integer default nextval('SequenzaMedico');
 create table Medico (
+	IdMedico char(8) not null ,
 	CF char(16) not null ,
-	Nome_Centro varchar(20) not null , 
-	Citta_centro varchar(20) not null , 
+	Centro char(8) not null ,
 	Nome varchar(20) not null ,
 	Cognome varchar(20) not null ,
-	Eta smallint not null check (eta > 0),
+	Data_nascita date not null ,
 	Citta varchar(20) not null ,
 	Indirizzo varchar(30) not null ,
-	Qualifica varchar(14) 
-		check (	qualifica = 'BASE' or
-			  	qualifica = 'SPECIALIZZATO') not null ,
-	MedicoId Medico_id,
-	constraint pk_medico primary key (MedicoId),
+	Qualifica char 
+		check (	qualifica = 'B' or
+			  	qualifica = 'S') not null ,
+	constraint pk_medico primary key (IdMedico),
 	constraint fk_medicocentro 
-			   foreign key (Nome_Centro,Citta_centro) references Centro_Vaccinale(Nome,Citta)
+			   foreign key (Centro) references Centro_Vaccinale(IdCentro)
 				on update cascade on delete set null,
 	unique(CF)
 );
 
 create table Preadesione (
-	CF char(16) not null ,
+	IDPread char(8) not null ,
 	Modalita char(3) check (modalita = 'app' or modalita = 'web') not null ,
 	Data_pre date not null ,
 	Ora time not null ,
 	Telefono varchar(15) ,
 	E_mail varchar(30) check (e_mail like '%@%.%') ,
-	constraint pk_preadesione primary key (CF),
+	constraint pk_preadesione primary key (IDPread),
 	constraint fk_preadesionevaccinando 
-				foreign key (CF) references vaccinando(CF)
+				foreign key (ID) references vaccinando(IDVacc)
 					on update cascade on delete cascade
 );
 
 create table Allergico (
-	Vaccinando char(16) not null ,
-	Allergia varchar(15) not null ,
+	Vaccinando char(8) not null ,
+	Allergia varchar(20) not null ,
 	constraint pk_allergico primary key (Vaccinando, Allergia),
 	constraint fk_allergicovaccinando
-				foreign key (vaccinando) references vaccinando(CF)
+				foreign key (vaccinando) references vaccinando(IDVacc)
 					on update cascade on delete cascade ,
 	constraint fk_allergicoallergia
 				foreign key (Allergia) references Allergia(nome)
 					on update cascade on delete cascade
 );
 
-create table Vaccinazione (
-	Vaccinando char(16) not null ,
-	Nome_Centro varchar(20) not null , 
-	Citta_centro varchar(20) not null ,
-	Data_vacc date not null ,
-	Ora time not null ,
-	Medico char(16) not null ,
-	constraint pk_vaccinazione primary key (Vaccinando,Data_vacc) ,
-	--unique (nome_centro, citta_centro, ora) ,
-	constraint fk_vaccinazionevaccinando
-				foreign key (vaccinando) references vaccinando(CF)
-					on update cascade on delete cascade ,
-	constraint fk_vaccinazionecentro 
-			   foreign key (nome_Centro,citta_centro) references Centro_Vaccinale(nome,citta)
-				on update cascade on delete cascade ,
-	constraint fk_vaccinazionemedico
-					foreign key (medico) references Medico(CF)
-						on update cascade on delete cascade
-);
-
 create table Convocazione (
-	Vaccinando char(16) not null ,
-	Nome_Centro varchar(20) not null , 
-	Citta_centro varchar(20) not null ,
+	IDConv char(8) not null ,
+	Vaccinando char(8) not null , 
+	Centro char(8) not null ,
 	Data_conv date not null ,
 	Ora time not null ,
 	Vaccino varchar(20) not null ,
-	constraint pk_convocazione primary key (Vaccinando,Data_conv) ,
-	--unique (nome_centro, citta_centro, ora) ,
+	constraint pk_convocazione primary key (IDConv) ,
+	unique (vaccinando,data_conv) ,
 	constraint fk_convocazionevaccinando
-				foreign key (vaccinando) references vaccinando(CF)
+				foreign key (vaccinando) references vaccinando(IDVacc)
 					on update cascade on delete cascade ,
 	constraint fk_convocazionecentro 
-			   foreign key (nome_Centro,citta_centro) references Centro_Vaccinale(nome,citta)
+			   foreign key (centro) references Centro_Vaccinale(IdCentro)
 				on update cascade on delete cascade ,
 	constraint fk_convocazionevaccino 
 			   foreign key (Vaccino) references Vaccino(nome)
@@ -190,13 +161,38 @@ create table Lotto (
 				on update cascade on delete cascade
 );
 
+create table Vaccinazione (
+	IDVacc char(8) not null ,
+	Vaccinando char(8) not null ,
+	Centro varchar(20) not null , 
+	Medico char(8) not null ,
+	Lotto char(8) not null ,
+	constraint pk_vaccinazione primary key (IdVacc) ,
+	--unique (nome_centro, citta_centro, ora) ,
+	constraint fk_vaccinazioneconvocazione 
+				foreign key (Idvacc) references Convocazione(IdConv)
+					on update cascade on delete cascade ,
+	constraint fk_vaccinazionevaccinando
+				foreign key (vaccinando) references vaccinando(IDVacc)
+					on update cascade on delete cascade ,
+	constraint fk_vaccinazionecentro 
+			   foreign key (centro) references Centro_Vaccinale(IdCentro)
+					on update cascade on delete cascade ,
+	constraint fk_vaccinazionemedico
+				foreign key (medico) references Medico(IDMedico)
+					on update cascade on delete cascade ,
+	constraint fk_vaccinazionelotto
+				foreign key (lotto) references Lotto(Numero)
+					on update cascade on delete cascade
+);
+
+
 create table Disponibilita_dosi (
-	Nome_Centro varchar(20) not null , 
-	Citta_centro varchar(20) not null ,
-	Lotto varchar(20) not null ,
-	constraint pk_dosi primary key (Nome_Centro,Citta_centro,Lotto) ,
+	Centro char(8) not null ,
+	Lotto char(8) not null ,
+	constraint pk_dosi primary key (Centro,Lotto) ,
 	constraint fk_dosicentro 
-			   foreign key (nome_Centro,citta_centro) references Centro_Vaccinale(nome,citta)
+			   foreign key (centro) references Centro_Vaccinale(IdCentro)
 				on update cascade on delete cascade ,
 	constraint fk_dosivaccino 
 			   foreign key (Lotto) references Lotto(numero)
@@ -216,13 +212,13 @@ create table Reazione_allergica (
 );
 
 create table Report (
-	Medico char(16) not null ,
+	Medico char(8) not null ,
 	Numero_lotto char(8) not null ,
-	Effetto_allergico varchar(15) not null ,
+	Reazione_riscontrata varchar(20) not null ,
 	Data_rep date not null ,
-	constraint pk_report primary key (Medico,Numero_lotto,Effetto_allergico),
+	constraint pk_report primary key (Medico,Numero_lotto,Reazione_riscontrata),
 	constraint fk_reportmedico
-				foreign key (medico) references Medico(CF)
+				foreign key (medico) references Medico(IdMedico)
 					on update cascade on delete cascade ,
 	constraint fk_reportlotto
 				foreign key (numero_lotto) references Lotto(Numero)
